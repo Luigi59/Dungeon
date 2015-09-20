cc8e8cf64006ec0eb4b945e2d967c5c5a6c7f0be
Estelle <Estelle@Estelle-HP>
2015-09-20 12:43:09 +0200

    normal room changement

--- Dungeon/src/main/NormalRoom.java
+++ Dungeon/src/main/NormalRoom.java
@@ -1,5 +1,18 @@
 package main;
 
-public class NormalRoom {
+public class NormalRoom extends Room {
 
+	public NormalRoom() {
+		super();
+	}
+	
+	@Override
+	public boolean canBeLeft() {
+		return true;
+	}
+	
+	@Override
+	public String getDescription() {
+		return " It's an intersection.";
+	}
 }
