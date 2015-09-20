efc78d9ae99f4d5433ef1267a3e83edf4ec28090
Estelle <Estelle@Estelle-HP>
2015-09-20 12:06:00 +0200

    Javadoc

---
+++ Dungeon/src/main/TrapRoom.java
@@ -0,0 +1,19 @@
+package dungeon;
+
+
+public class TrapRoom extends Room {
+
+	public TrapRoom() {
+		super();
+	}
+	
+	@Override
+	public boolean canBeLeft() {
+		return false;
+	}
+	
+	@Override
+	public String getDescription() {
+		return "It's a trap!";
+	}
+}
