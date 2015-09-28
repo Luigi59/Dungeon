3c6665c166bd071fca2977a2534c5303a7d651df
Estelle <Estelle@Estelle-HP>
2015-09-28 19:22:28 +0200

    changement de nom

---
+++ Dungeon/src/dungeon/objects/Item.java
@@ -0,0 +1,24 @@
+package dungeon.objects;
+
+public abstract class Item {
+
+	private String nameObject;
+	
+	public Item(String nameObject) {
+		this.nameObject = nameObject;
+	}
+	
+	/**
+	 * gives the name of the object
+	 * @return the name of the object
+	 */
+	public String getName() {
+		return this.nameObject;
+	}
+	
+	/**
+	 * gives the description of the object
+	 * @return the description of the object
+	 */
+	public abstract String toString();
+}
